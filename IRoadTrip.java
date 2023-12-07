61c8f26b656737150ac777ce3b3f674919dbdae5
Deep Patel <dapatel103@Deeps-MacBook-Pro.local>
2023-12-06 21:56:23 -0800

    project3 commit

--- IRoadTrip.java
+++ IRoadTrip.java
@@ -1,36 +1,240 @@
-import java.util.List;
-
+import java.io.File;
+import java.util.*;
 public class IRoadTrip {
-
-
+    ArrayList<String> mCountryList;
+    Map<String, Map<String, Integer>>  mAdjustMap = new HashMap<>();
     public IRoadTrip (String [] args) {
         // Replace with your code
+        if(args.length != 3){
+            System.err.println("input argument is invalud");
+            System.exit(1);
+        }
+        //read sate_name.csv
+        Map<Integer, String>  nameMap = new HashMap<>();
+        Scanner fScan;
+
+        try {
+            fScan = new Scanner(new File(args[2]));
+            fScan.nextLine();
+            while (fScan.hasNextLine()) {
+                String[] row = fScan.nextLine().split("\t");
+                if(row[0].equals("678")){
+                    System.out.println();
+                }
+                if(row[4].equals("2020-12-31")){
+                    nameMap.put(Integer.parseInt(row[0]), validName(row[2]));
+                }
+            }
+            fScan.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        mCountryList = new ArrayList<>(nameMap.values());
+        // read borders.txt
+        try {
+            fScan = new Scanner(new File(args[0]));
+            while (fScan.hasNextLine()) {
+                String[] row = fScan.nextLine().split(" = ");
+                String startCountry = validName(row[0]);
+                if(!mCountryList.contains(startCountry))
+                    continue;
+                mAdjustMap.put(startCountry, new HashMap<>());
+                if(row.length == 2){
+                    String[] subRow = row[1].split("; ");
+                    for(String subline : subRow){
+                        String[] words = subline.split(" ");
+                        String connection = "";
+                        for(int i = 0 ; i < words.length - 2; i++)
+                            connection += words[i] + " ";
+                        connection = validName(connection);
+                        if(!mCountryList.contains(connection))
+                            continue;
+                        mAdjustMap.get(startCountry).put(connection, -1);
+                    }
+                }
+            }
+            fScan.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        //read capdist.csv
+        try {
+            fScan = new Scanner(new File(args[1]));
+            fScan.nextLine();
+            while (fScan.hasNextLine()) {
+                String[] row = fScan.nextLine().split(",");
+                String firstCountry = getCountryName(row[0], nameMap);
+                String secondCountry = getCountryName(row[2], nameMap);
+                if(firstCountry == null || secondCountry == null){
+                    continue;
+                }
+                if(firstCountry.isEmpty() || secondCountry.isEmpty())
+                    continue;
+                if(!mAdjustMap.keySet().contains(firstCountry))
+                    continue;
+                if(!mAdjustMap.get(firstCountry).keySet().contains(secondCountry))
+                    continue;
+                mAdjustMap.get(firstCountry).put(secondCountry, Integer.parseInt(row[4]));
+            }
+            fScan.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
     }
-
-
+    private String validName(String content){
+        content = content.trim().split("\\(")[0].trim();
+        String result = content.trim().split("/")[0].trim();
+        if(result.equals("Korea, North") || result.equals("Korea, People's Republic of"))
+            return "North Korea";
+        if(result.equals("Korea, South") || result.equals("Korea, Republic of"))
+            return "South Korea";
+        if(result.equals("Congo, Democratic Republic of") || result.equals("Congo, Democratic Republic of the"))
+            return "Democratic Republic of the Congo";
+        if(result.equals("Republic of the Congo") || result.equals("Congo, Republic of the"))
+            return "Congo";
+        if(result.equals("Vietnam, Democratic Republic of"))
+            return "Vietnam";
+        if(result.equals("German Federal Republic"))
+            return "Germany";
+        if(result.equals("Macedonia"))
+            return "North Macedonia";
+        if(result.equals("Myanmar"))
+            return "Burma";
+        return result;
+    }
+    private String getCountryName(String strNum, Map<Integer, String>  nameMap){
+        try{
+            int num = Integer.parseInt(strNum);
+            return nameMap.get(num);
+        }catch (Exception e){
+            return "";
+        }
+    }
+    class Node {
+        String name;
+        int distance;
+        public Node(String n, int d) {
+            name = n;
+            distance = d;
+        }
+    };
     public int getDistance (String country1, String country2) {
         // Replace with your code
-        return -1;
+        if(!mCountryList.contains(country1) || !mCountryList.contains(country2))
+            return -1;
+        if(!mAdjustMap.keySet().contains(country1))
+            return -1;
+        if(!mAdjustMap.get(country1).keySet().contains(country2))
+            return -1;
+        return mAdjustMap.get(country1).get(country2);
     }
-
-
     public List<String> findPath (String country1, String country2) {
         // Replace with your code
-        return null;
+        PriorityQueue<Node> minHeap = new PriorityQueue<>((lhs, rhs) -> lhs.distance - rhs.distance);
+        minHeap.add(new Node(country1, 0));
+        Map<String, Integer> dist = new HashMap<>();
+        Map<String, String> prev = new HashMap<>();
+        Map<String, Boolean> done = new HashMap<>();
+        for(String name: mCountryList){
+            dist.put(name, Integer.MAX_VALUE);
+            prev.put(name, "");
+            done.put(name, false);
+        }
+        done.put(country1, true);
+        dist.put(country1, 0);
+
+        while (!minHeap.isEmpty()) {
+            Node node = minHeap.poll();
+            String country = node.name;
+            if(country.equals(country2)){
+                break;
+            }
+            if(!mAdjustMap.keySet().contains(country)){
+                continue;
+            }
+
+            for (Map.Entry<String, Integer> entry : mAdjustMap.get(country).entrySet())
+            {
+                String key = entry.getKey();
+                int dis = entry.getValue();
+                if(dis == -1)
+                    continue;
+
+                if (!done.get(key) && (dist.get(country) + dis) < dist.get(key)) {
+                    dist.put(key, dist.get(country) + dis);
+                    minHeap.add(new Node(key, dist.get(key)));
+                    prev.put(key, country);
+                }
+            }
+            done.put(country, true);
+        }
+
+        ArrayList<String> path = new ArrayList<>();
+        String con = country2;
+        while(!con.isEmpty()){
+            path.add(con);
+            con = prev.get(con);
+        }
+        Collections.reverse(path);
+        ArrayList<String> result = new ArrayList<>();
+        if(path.size() == 0)
+            return result;
+
+        if(path.size() == 1){
+            if(country1.equals(country2))
+                result.add(country1 + " --> " + country2 + " (0 km.)");
+            return result;
+        }
+
+        for(int i = 0 ; i < path.size() - 1 ; i++){
+            String con1 = path.get(i);
+            String con2 = path.get(i+1);
+            result.add(con1 + " --> " + con2 + " ("+getDistance(con1, con2)+" km.)");
+        }
+        return result;
     }
-
-
+    private String acceptCountry(Scanner scan, String order){
+        String countryName = "";
+        while(true){
+            System.out.println("Enter the name of the "+order+" country (type EXIT to quit):");
+            countryName = scan.nextLine();
+            if(countryName.equals("EXIT"))
+                return "";
+            if(!mCountryList.contains(countryName)){
+                System.out.println("Invalid country name. Please enter a valid country name. ");
+            }
+            break;
+        }
+        return countryName;
+    }
     public void acceptUserInput() {
         // Replace with your code
         System.out.println("IRoadTrip - skeleton");
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String country1 = acceptCountry(scanner, "first");
+            if(country1.isEmpty())
+                break;
+            String country2 = acceptCountry(scanner, "second");
+            if(country2.isEmpty())
+                break;
+            List<String> path = findPath(country1, country2);
+            System.out.println("Route from "+country1+" to "+country2+":");
+            for(String item : path){
+                System.out.println("* " + item);
+            }
+        }
     }
-
 
     public static void main(String[] args) {
         IRoadTrip a3 = new IRoadTrip(args);
 
         a3.acceptUserInput();
     }
-
 }
-
--- README.md
+++ README.md
@@ -1,5 +1,71 @@
 # CS 245 (Fall 2023) - Assignment 3 - IRoadTrip
 
-Can you plan the path for a road trip from one country to another?
+a) Design choices and OO decomposition of the problem:
+The design of the `IRoadTrip` class follows an Object-Oriented (OO) approach to decompose the problem. Here are the design choices and decomposition of the problem:
 
-Change the java source code, but do not change the data files. See Canvas for assignment details.
+1. Class Structure:
+   - The `IRoadTrip` class is the main class that contains the implementation of the road trip functionality.
+   - It has instance variables `mCountryList` and `mAdjustMap` for storing country information and adjacency matrix, respectively.
+   - The class also contains helper classes `Node` for representing nodes in the graph.
+
+2. Constructor:
+   - The `IRoadTrip` class has a constructor that takes an array of strings `args` as input.
+   - The constructor initializes the country list and adjacency map by reading data from files specified in the `args` array.
+
+3. Private Helper Methods:
+   - The class contains several private helper methods to assist in various tasks such as data preprocessing and validation.
+   - These methods include `validName`, `getCountryName`, and others.
+   - These methods are responsible for cleaning up country names, retrieving country names from a map, and performing other necessary operations.
+
+4. Public Methods:
+   - The class provides two public methods: `getDistance` and `findPath`.
+   - The `getDistance` method takes two country names as input and returns the distance between them based on the adjacency map.
+   - The `findPath` method takes two country names as input and finds the shortest path between them using Dijkstra's algorithm.
+   - Both methods make use of the adjacency map and other helper methods to perform their respective tasks.
+
+5. User Input Handling:
+   - The class includes a method `acceptUserInput` that prompts the user to enter country names and finds the shortest path between them.
+   - This method uses a `Scanner` object to read user input and repeatedly calls the `findPath` method to display the shortest path.
+
+b) Data structure choices:
+The `IRoadTrip` class uses the following data structures:
+
+1. ArrayList:
+   - The `mCountryList` variable is an ArrayList that stores the list of countries.
+   - It is used to validate country names and check for their existence.
+
+2. HashMap:
+   - The `mAdjustMap` variable is a HashMap that represents the adjacency map.
+   - It stores country connections and distances between them.
+   - The keys of the outer map represent starting countries, and the values are inner maps.
+   - The keys of the inner maps represent destination countries, and the values represent the distances between them.
+   - This data structure allows efficient retrieval of distances between countries.
+
+3. PriorityQueue:
+   - The `minHeap` variable is a PriorityQueue that is used in the Dijkstra's algorithm implementation.
+   - It stores nodes (countries) based on their distances, with the node having the smallest distance at the front.
+   - This allows efficient retrieval of the node with the minimum distance during the algorithm execution.
+
+4. Maps:
+   - Several `Map` objects are used to store various mappings required for data processing and algorithm execution.
+   - Examples include `nameMap` for mapping country IDs to names, `dist` for storing distances during the algorithm, and others.
+   - Maps provide efficient key-value lookups, which are useful for retrieving data during the execution of the program.
+
+c) Anything else you believe might be useful:
+The provided source code implements a road trip functionality using graph traversal algorithms. It reads data from input files and constructs an adjacency map to represent country connections and distances. The Dijkstra's algorithm is then used to find the shortest path between two countries.
+
+To enhance the functionality and usability of the program, you could consider the following:
+
+1. Error Handling:
+   - The current implementation does minimal error handling. You can add more robust error handling and exception handling to handle various scenarios, such as file reading errors, invalid input, or incorrect file formats.
+
+2. Input Validation:
+   - You can enhance the input validation process by checking for valid country names and handling cases where the user enters non-existent countries.
+
+3. User Interface:
+   - Currently, the user interaction is limited to the console. You can consider developing a graphical user interface (GUI) to provide a more user-friendly and intuitive way for users to input country names and view the shortest path.
+
+4. Optimize Performance:
+   - Depending on the size of the input data, there may be opportunities to optimize the performance of the program. You can analyze the code and identify areas where performance improvements can be made, such as optimizing data structures or algorithms.
+
+Overall, I implement a road trip functionality using graph traversal algorithms. 
